8a3863e5d95e5990177c61c20e09ed81ec725a32
Ryan Kramer <default.kramer@gmail.com>
2025-08-31 15:52:23 -0500

    revert temp changes

--- Scripts/SaveData/StageData.cs
+++ Scripts/SaveData/StageData.cs
@@ -13,11 +13,10 @@ using System.Security.Cryptography.X509Certificates;
 using EyeOfRubiss.Info;
 using EyeOfRubiss.Nodes;
 using System.Data.SqlTypes;
-using EyeOfRubiss.Integration;
 
 namespace EyeOfRubiss
 {
-    public class StageData : SaveData, EyeOfRubiss.Integration.IWorld
+    public class StageData : SaveData
     {
         /*
             Here follows a general map of the buffer's layout.
@@ -334,8 +333,6 @@ namespace EyeOfRubiss
             public bool PlayerPlaced { get => SaveData.GetBit(Address + 1, 3); set => SaveData.SetBit(Address + 1, 3, value); }
             public ChiselType Chisel { get => (ChiselType)SaveData.GetNumberBitwise(Address + 1, 4, 4); set => SaveData.SetNumberBitwise(Address + 1, 4, 4, (byte)value); }
 
-            internal Block AsIntegrationBlock => new Block((ushort)SaveData.GetNumberBitwise(Address, 0, 16));
-
             public BlockInfo GetInfo() => BlockInfo.Get(BlockID);
 
             public enum ChiselType : byte
@@ -384,19 +381,6 @@ namespace EyeOfRubiss
         {
             return GetChunk(PositionToChunkIndex(position));
         }
-
-        bool IWorld.HasData(Box box)
-        {
-            // We're lucky that the Zylann's boxes line up with DQB2 chunks such that
-            // we only have to check the start position of the box.
-            // (They are 16x16x16 and thus will never exist in more than one 32x32x96 chunk.)
-            var chunkIndex = PositionToChunkIndex(box.Start);
-            return chunkIndex >= 0 && chunkIndex < Chunk.MAXIMUM && GetChunk(chunkIndex).IsUsed();
-        }
-
-        Block IWorld.GetBlockAtPosition(Vector3I position) => GetBlockAtPosition(position).AsIntegrationBlock;
-
-		Vector3I IWorld.InitialCameraPosition => new Vector3I(1024, 96, 1024);
 
         public Chunk AddChunk(int index, ushort? id = null)
         {
